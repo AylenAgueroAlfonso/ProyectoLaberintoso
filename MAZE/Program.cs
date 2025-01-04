094487c6347230b57e246112c0f6411383f8bf40
aylen aguero alfonso <aylen@gmail.com>
2025-01-04 10:33:18 -0500

    Inicio del proyecto

---
+++ MAZE/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int [,] Maze = new int [3,3];
+
+int b = Maze[0,1];
+
+Console.WriteLine("Hay " + b );
+
+
+
+
+/*
+for(int i = 0; i < 3; i++)
+{
+    for(int j = 0; j < 3; j++)
+    {
+        if()
+    }
+}
+*/
