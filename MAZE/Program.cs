7619653934553f98213c00481dfef05c84f0213d
aylen aguero alfonso <aylen@gmail.com>
2025-01-04 11:27:58 -0500

    Intentando crear un métod q genere aleatoriamente un laberinto?

--- MAZE/Program.cs
+++ MAZE/Program.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 namespace Laberinto 
 {
+   class LaberintoGenerador 
+   {
+
+   }
+
+
+
     class Programa
     {
         static void Main ()
         {
+            int[,] Maze = new int[3, 3];
 
+int n = Maze.GetLength(0);
+int m = Maze.GetLength(1);
+
+for (int fila = 0; fila < n; fila++)
+{
+    for (int colum = 0; colum < m; colum++)
+    {
+        if (Maze[fila,colum] == 0)
+        {
+            Maze[fila,colum] = 1;
+
+        } 
+        }
+}
         }
     }
 }
--- MAZE/obj/Debug/net8.0/MAZE.AssemblyInfo.cs
+++ MAZE/obj/Debug/net8.0/MAZE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MAZE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79290127abcdcbd7693a7feafafc132f94d1a0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ad9d743028e6955b3ddd6910c8b837dfcfab189")]
 [assembly: System.Reflection.AssemblyProductAttribute("MAZE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MAZE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
